65711ae136a729bfc3aa4f027431d1dbf30af09d
numair1 <numair1@users.noreply.github.com>
2023-06-05 16:41:36 -0400

    Add subsampled data used for FCI

---
+++ birds/causal_structure_learning/subsample_data.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-n", type = int)
+parser.add_argument("-f")
+parser.add_argument("-o")
+args = parser.parse_args()
+
+data = pd.read_csv(args.f, sep = "\t")
+data = data.drop(['image_name', 'y_true'], axis = 1)
+n_subsamples = args.n
+for i in range(n_subsamples):
+    i_subsample = data.sample(frac = 1, replace = True)
+    i_subsample.to_csv(args.o + "/subsample_"+str(i)+".csv",sep = '\t', index = False)
